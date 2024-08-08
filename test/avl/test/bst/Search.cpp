857b958cc9b4597c982cdf2b9ded7bd659bde68a
vityaman <vityaman.dev@yandex.ru>
2024-08-08 16:33:33 +0300

    #3 Test Search algorithm

---
+++ test/avl/test/bst/Search.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "avl/BST.hpp"
+#include "avl/test/bst/Example.hpp"
+
+namespace avl::test {
+
+TEST(BSTSearch, Simple) {
+  Nodes bst;
+
+  auto* tree = bst(5, bst(4, bst(3)), bst(8, bst(6), bst(9)));
+
+  ASSERT_NE(Search(tree, 3), nullptr);
+  ASSERT_NE(Search(tree, 4), nullptr);
+  ASSERT_NE(Search(tree, 5), nullptr);
+  ASSERT_NE(Search(tree, 6), nullptr);
+  ASSERT_NE(Search(tree, 8), nullptr);
+  ASSERT_NE(Search(tree, 9), nullptr);
+
+  ASSERT_EQ(Search(tree, 0), nullptr);
+  ASSERT_EQ(Search(tree, 2), nullptr);
+  ASSERT_EQ(Search(tree, 7), nullptr);
+  ASSERT_EQ(Search(tree, 10), nullptr);
+}
+
+TEST(BSTSearch, Singleton) {
+  Node node = {.key = 5};
+  ASSERT_EQ(Search(&node, 5), &node);
+  ASSERT_EQ(Search(&node, 0), nullptr);
+  ASSERT_EQ(Search(&node, 10), nullptr);
+}
+
+TEST(BSTSearch, Nil) {
+  ASSERT_EQ(Search<Node>(nullptr, 0), nullptr);
+}
+
+}  // namespace avl::test
