f1eb677a1554e3f222d4137e0397f9ef508792ae
vityaman <vityaman.dev@yandex.ru>
2024-08-08 16:33:46 +0300

    #3 Test Min and Max algorithms

---
+++ test/avl/test/bst/MinMax.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "avl/BST.hpp"
+#include "avl/test/bst/Example.hpp"
+
+namespace avl::test {
+
+TEST(BSTMin, Example) {
+  auto [nodes, root] = Example();
+  ASSERT_EQ(Min(Search(root, 8)), Search(root, 1));
+  ASSERT_EQ(Min(Search(root, 3)), Search(root, 1));
+  ASSERT_EQ(Min(Search(root, 1)), Search(root, 1));
+  ASSERT_EQ(Min(Search(root, 6)), Search(root, 4));
+  ASSERT_EQ(Min(Search(root, 4)), Search(root, 4));
+  ASSERT_EQ(Min(Search(root, 7)), Search(root, 7));
+  ASSERT_EQ(Min(Search(root, 10)), Search(root, 10));
+  ASSERT_EQ(Min(Search(root, 14)), Search(root, 13));
+  ASSERT_EQ(Min(Search(root, 13)), Search(root, 13));
+}
+
+TEST(BSTMax, Example) {
+  auto [nodes, root] = Example();
+  ASSERT_EQ(Max(Search(root, 8)), Search(root, 14));
+  ASSERT_EQ(Max(Search(root, 3)), Search(root, 7));
+  ASSERT_EQ(Max(Search(root, 1)), Search(root, 1));
+  ASSERT_EQ(Max(Search(root, 6)), Search(root, 7));
+  ASSERT_EQ(Max(Search(root, 4)), Search(root, 4));
+  ASSERT_EQ(Max(Search(root, 7)), Search(root, 7));
+  ASSERT_EQ(Max(Search(root, 10)), Search(root, 14));
+  ASSERT_EQ(Max(Search(root, 14)), Search(root, 14));
+  ASSERT_EQ(Max(Search(root, 13)), Search(root, 13));
+}
+
+}  // namespace avl::test
