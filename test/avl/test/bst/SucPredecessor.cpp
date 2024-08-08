0faad0e69e38a2a15064c6c859fc87e42c1a3609
vityaman <vityaman.dev@yandex.ru>
2024-08-08 16:34:14 +0300

    #3 Test Successor and Predecessor algorithms

---
+++ test/avl/test/bst/SucPredecessor.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "avl/BST.hpp"
+#include "avl/test/bst/Example.hpp"
+
+namespace avl::test {
+
+TEST(BSTSuccessor, Example) {
+  auto [nodes, root] = Example();
+  ASSERT_EQ(Successor(Search(root, 1)), Search(root, 3));
+  ASSERT_EQ(Successor(Search(root, 3)), Search(root, 4));
+  ASSERT_EQ(Successor(Search(root, 4)), Search(root, 6));
+  ASSERT_EQ(Successor(Search(root, 6)), Search(root, 7));
+  ASSERT_EQ(Successor(Search(root, 7)), Search(root, 8));
+  ASSERT_EQ(Successor(Search(root, 8)), Search(root, 10));
+  ASSERT_EQ(Successor(Search(root, 10)), Search(root, 13));
+  ASSERT_EQ(Successor(Search(root, 13)), Search(root, 14));
+  ASSERT_EQ(Successor(Search(root, 14)), nullptr);
+}
+
+TEST(BSTPredecessor, Example) {
+  auto [nodes, root] = Example();
+  ASSERT_EQ(Predecessor(Search(root, 1)), nullptr);
+  ASSERT_EQ(Predecessor(Search(root, 3)), Search(root, 1));
+  ASSERT_EQ(Predecessor(Search(root, 4)), Search(root, 3));
+  ASSERT_EQ(Predecessor(Search(root, 6)), Search(root, 4));
+  ASSERT_EQ(Predecessor(Search(root, 7)), Search(root, 6));
+  ASSERT_EQ(Predecessor(Search(root, 8)), Search(root, 7));
+  ASSERT_EQ(Predecessor(Search(root, 10)), Search(root, 8));
+  ASSERT_EQ(Predecessor(Search(root, 13)), Search(root, 10));
+  ASSERT_EQ(Predecessor(Search(root, 14)), Search(root, 13));
+}
+
+}  // namespace avl::test
