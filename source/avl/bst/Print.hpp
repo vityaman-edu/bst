1a5cfb98d11eee0ade54c67990468f9b8d0fd44c
vityaman <vityaman.dev@yandex.ru>
2024-08-15 09:17:31 +0300

    #3 Add print BST procedure

---
+++ source/avl/bst/Print.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ostream>
+
+#include "avl/bst/Node.hpp"
+#include "avl/bst/Tree.hpp"
+
+namespace avl {
+
+template <BSTTree Tree, BSTNode Node = typename Tree::Node>
+void Print(std::ostream& out, Tree& tree, Node* node = nullptr, std::size_t indent = 0) {
+  if (node == nullptr) {
+    node = tree.Root();
+  }
+
+  if (node == tree.Nil()) {
+    return;
+  }
+
+  Print(out, tree, Child(Side::RIGHT, node), indent + 1);
+  out << std::string(2 * indent, ' ') << "(" << node->key << ")" << std::endl;
+  Print(out, tree, Child(Side::LEFT, node), indent + 1);
+}
+
+}  // namespace avl
