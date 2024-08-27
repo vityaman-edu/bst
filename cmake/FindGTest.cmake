9515218c9bd413bd92e5393aef5bb29e18b9f13e
vityaman <vityaman.dev@yandex.ru>
2024-08-27 14:51:07 +0300

    #2 Fix GTest downloading

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,7 +1,5 @@
 include(FetchContent)
 
-set(FETCHCONTENT_FULLY_DISCONNECTED ON)
-
 FetchContent_Declare(
   googletest
   URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
